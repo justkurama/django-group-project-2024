cdd855c4b17ac3d02428eeeeadb0f5c635b24bc4
Altynbek38 <altynbek4649@gmail.com>
2024-10-13 15:29:48 +0500

    booking models

---
+++ myproject/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+# Create your models here.
+class User(AbstractBaseUser):
+    pass 
+
+class Profile(models.Model):
+    pass
