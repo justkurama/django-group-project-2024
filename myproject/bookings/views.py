30c2bdc53c565b96bdb063628411702a4c268ad5
Altynbek38 <altynbek4649@gmail.com>
2024-10-15 15:11:19 +0500

    booking view functions

--- myproject/bookings/views.py
+++ myproject/bookings/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def booking_create(request, listing_id, room_id):
+    pass
+
+def booking_detail(request, booking_id):
+    pass
+
+def booking_list(request):
+    pass
+
+def payment(request, booking_id):
+    pass
+
+def review_create(request, listing_id):
+    pass
+
+def review_list(request, listing_id):
+    pass
+
+def review_update(request, review_id):
+    pass
+
+def review_delete(request, review_id):
+    pass
